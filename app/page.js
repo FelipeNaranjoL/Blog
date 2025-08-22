3e4c3c777b9d148a1149255234243bc17931bc92
FelipeNaranjoL <fe.naranjo@duocuc.cl>
2025-08-21 21:11:01 -0400

    Listado de blogs listos y con filtro

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 // importaciones de componentes o librerias
+'use client'
 import Header from "@/components/Header";
+
+import BlogList from "@/components/BlogList";
 
 
 // Archivo principal donde se subiran los componentes y servira como index o main
 export default function Home() {
   return (
-    // componente header
+    <>
+    {/* // componente header */}
     <Header />
+    {/* // componente BlogList */}
+    <BlogList/>
+    </>
   );
 }
 
